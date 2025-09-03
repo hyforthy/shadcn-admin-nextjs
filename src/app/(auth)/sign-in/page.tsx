e7f7f74583bbb539b72b394f726118d67beb4ca3
hyforthy <hyforthy@gmail.com>
2025-09-03 21:35:30 +0800

    del clerk and build OK

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link";
 import {
   Card,
   CardContent,
@@ -6,20 +6,20 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
-import { UserAuthForm } from './components/user-auth-form'
+} from "@/components/ui/card";
+import { UserAuthForm } from "./components/user-auth-form";
 
 export default function SignIn() {
   return (
-    <Card className='gap-4'>
+    <Card className="gap-4">
       <CardHeader>
-        <CardTitle className='text-lg tracking-tight'>Sign In</CardTitle>
+        <CardTitle className="text-lg tracking-tight">Sign In</CardTitle>
         <CardDescription>
           Enter your email and password below to log into your account. <br />
-          Don&apos;t have an account?{' '}
+          Don&apos;t have an account?{" "}
           <Link
-            href='/sign-up'
-            className='hover:text-primary underline underline-offset-4'
+            href="/sign-up"
+            className="hover:text-primary underline underline-offset-4"
           >
             Sign Up
           </Link>
@@ -29,18 +29,18 @@ export default function SignIn() {
         <UserAuthForm />
       </CardContent>
       <CardFooter>
-        <p className='text-muted-foreground px-8 text-center text-sm'>
-          By clicking sign in, you agree to our{' '}
+        <p className="text-muted-foreground px-8 text-center text-sm">
+          By clicking sign in, you agree to our{" "}
           <a
-            href='/terms'
-            className='hover:text-primary underline underline-offset-4'
+            href="/terms"
+            className="hover:text-primary underline underline-offset-4"
           >
             Terms of Service
-          </a>{' '}
-          and{' '}
+          </a>{" "}
+          and{" "}
           <a
-            href='/privacy'
-            className='hover:text-primary underline underline-offset-4'
+            href="/privacy"
+            className="hover:text-primary underline underline-offset-4"
           >
             Privacy Policy
           </a>
@@ -48,5 +48,5 @@ export default function SignIn() {
         </p>
       </CardFooter>
     </Card>
-  )
+  );
 }
--- src/app/clerk/_authenticated/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthenticatedLayout } from '@/components/layout/authenticated-layout'
-
-export default function ClerkAuthenticatedLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <AuthenticatedLayout>{children}</AuthenticatedLayout>
-}
